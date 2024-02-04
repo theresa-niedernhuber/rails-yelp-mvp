fe9d6395520b28a49e4182aac8ffe5390479329b
Theresa Niedernhuber <theresa-niedernhuber@live.de>
2024-02-04 19:09:07 +0100

    review model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_04_180551) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_04_180837) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone_number"
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
